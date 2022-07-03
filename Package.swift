8f67cc33846596ccdc4bd26fa6b11e1c4a3542db
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 20:47:51 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -5,6 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Spring",
+    platforms: [.macOS(.v10_10),
+                .iOS(.v9),
+                .tvOS(.v9),
+                .watchOS(.v2)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Spring",
+            targets: ["Spring"]),
+    ],
     targets: [
         .target(name: "Spring"),
     ]
