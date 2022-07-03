3c1c90332bfb79b56aa63da9b8e07ce7acaed48a
s-higuchi <47686127+hsin5@users.noreply.github.com>
2022-07-03 20:46:23 +0900

    chore: SwiftPM

--- Package.swift
+++ Package.swift
@@ -6,8 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Spring",
     targets: [
-        .target(
-            name: "Spring",
-            path: "./"),
+        .target(name: "Spring"),
     ]
 )
